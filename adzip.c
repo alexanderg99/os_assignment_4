2ec5e690efa5087b3d9ae1adb949b1bc4fe9833c
Alexander Gunawan <58327698+silversunr1se@users.noreply.github.com>
2023-05-09 19:20:49 +0400

    ignore adzip2. this new file should archive correctly.

--- adzip.c
+++ adzip.c
@@ -6,9 +6,10 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <dirent.h>
+#include <libgen.h>
 
-//test
-// Define constants and structures
+//create header structure
 #define HEADER_SIGNATURE 0xADADADAD
 #define MAX_PATH_LEN 256
 
@@ -19,7 +20,6 @@ typedef struct {
     uint32_t metadata_offset;// Offset to the metadata section in the archive
 } Header;
 
-// Metadata structure for files and directories in the archive
 typedef struct {
     char name[MAX_PATH_LEN]; // Name of the file or directory
     uint8_t type;            // Type of the entry (e.g., regular file or directory)
@@ -31,159 +31,234 @@ typedef struct {
     uint32_t content_offset; // Offset to the file content in the archive
 } Metadata;
 
-void print_directory(FILE *archive, Metadata *parent_metadata, int depth) {
-    for (int i = 0; i < depth; i++) {
-        printf("  ");
+void write_file_to_archive(FILE *archive, const char *file_path, uint32_t *header_offset, uint32_t *metadata_offset, int* num_entries) {
+    // Open the file
+    int fd = open(file_path, O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
     }
-    printf("%s\n", parent_metadata->path);
 
-    for (int i = 0; i < parent_metadata->size; i++) {
-        Metadata metadata;
-        fread(&metadata, sizeof(Metadata), 1, archive);
+    // Get the file size
+    struct stat st;
+    if (fstat(fd, &st) == -1) {
+        perror("Error getting file size");
+        exit(EXIT_FAILURE);
+    }
+    uint32_t file_size = st.st_size;
 
-        if (metadata.type == DT_DIR) {
-            print_directory(archive, &metadata, depth + 1);
-        } else {
-            for (int j = 0; j < depth + 1; j++) {
-                printf("  ");
-            }
-            printf("%s\n", metadata.path);
-        }
+    //Get the metadata of the file with stat
+    struct stat file_stat;
+    if (stat(file_path, &file_stat) == -1) {
+        perror("Error getting file metadata");
+        exit(EXIT_FAILURE);
+    }
+
+    //Initialize a metadata struct and populate it with the relevant details
+    Metadata metadata;
+    strcpy(metadata.name, file_path);
+    metadata.type = DT_REG;
+    strcpy(metadata.path, file_path);
+    metadata.owner = file_stat.st_uid;
+    metadata.group = file_stat.st_gid;
+    metadata.rights = file_stat.st_mode;
+    metadata.size = file_size;
+    metadata.content_offset = *header_offset;
+    //metadata.content_offset = 0;
+
+    //print the metadata content offset and the metadata name
+    printf("Writing Content at Position: %d\n", metadata.content_offset);
+    //print the metadata name
+    printf("metadata name: %s\n", metadata.name);
+    printf("Writing Metadata at Position: %d\n", *metadata_offset);
+
+
+
+
+    // Allocate a buffer for the file content
+    char *buffer = malloc(file_size);
+    if (buffer == NULL) {
+        perror("Error allocating buffer");
+        exit(EXIT_FAILURE);
+    }
+
+    // Read the file content into the buffer
+    if (read(fd, buffer, file_size) == -1) {
+        perror("Error reading file");
+        exit(EXIT_FAILURE);
+    }
+
+    // Write the file content to the archive
+    fseek(archive, metadata.content_offset, SEEK_SET);
+    if (fwrite(buffer, file_size, 1, archive) != 1) {
+        perror("Error writing to archive");
+        exit(EXIT_FAILURE);
+    }
+
+    //Go to position in archive where metadata should be written
+    fseek(archive, *metadata_offset, SEEK_SET);
+    // Write the metadata to the archive
+    if (fwrite(&metadata, sizeof(Metadata), 1, archive) != 1) {
+        perror("Error writing to archive");
+        exit(EXIT_FAILURE);
+    }
+
+
+    // Update the offset
+    *header_offset += file_size;
+    *metadata_offset += sizeof(Metadata);
+
+    //update num entries
+    *num_entries += 1;
+
+    // Close the file
+    if (close(fd) == -1) {
+        perror("Error closing file");
+        exit(EXIT_FAILURE);
     }
 }
 
-void print_hierarchy(FILE *archive) {
-    Header header;
-    fread(&header, sizeof(Header), 1, archive);
+void traverse_directory(FILE *archive_path, const char *dir_path, const char *base_path, uint32_t *header_offset, uint32_t *metadata_offset, int* num_entries) {
 
-    if (header.signature != HEADER_SIGNATURE) {
-        fprintf(stderr, "Invalid archive file.\n");
-        return;
-    }
-    //print the header object
-    printf("header.signature: %d\n", header.signature);
-    printf("header.num_entries: %d\n", header.num_entries);
-    printf("header.metadata_offset: %d\n", header.metadata_offset);
-
-    fseek(archive, header.metadata_offset, SEEK_SET);
-
-    Metadata root_metadata;
-    fread(&root_metadata, sizeof(Metadata), 1, archive);
-
-    if (root_metadata.type == DT_DIR) {
-        print_directory(archive, &root_metadata, 0);
-    } else {
-        printf("%s\n", root_metadata.path);
-    }
-}
-
-void display_hierarchy(const char *archive_path) {
-    FILE *archive = fopen(archive_path, "rb");
-    if (!archive) {
-        perror("fopen");
-        return;
-    }
-
-    printf("Archive: %s\n", archive_path);
-    print_hierarchy(archive);
-
-    fclose(archive);
-}
-
-
-// Function to traverse a directory recursively and store its contents in the archive
-void store_directory(FILE *archive, const char *dir_path, int *metadata_offset, int *data_offset) {
-    // Open the directory
+    //printf("dictionary travelled\n");
     DIR *dir = opendir(dir_path);
-    if (!dir) {
-        perror("opendir");
-        return;
+    if (dir == NULL) {
+        perror("Error opening directory");
+        exit(EXIT_FAILURE);
     }
-    printf("dir_path: %s\n", dir_path);
 
     struct dirent *entry;
-    // Iterate over directory entries
-    while ((entry = readdir(dir))) {
-
-        // Skip "." and ".." entries
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
-            continue;
-        }
-
-        // Create the full path for the current entry
-        char entry_path[MAX_PATH_LEN];
-        snprintf(entry_path, sizeof(entry_path), "%s/%s", dir_path, entry->d_name);
-        printf("entry_path: %s\n", entry_path);
-
-        // Get the entry's metadata (e.g., file type, owner, group, rights)
-        struct stat st;
-        if (stat(entry_path, &st) == -1) {
-            perror("stat");
-            continue;
-        }
-        //print the entry's metadata
-        printf("st_uid: %d\n", st.st_uid);
-        printf("st_gid: %d\n", st.st_gid);
-        printf("st_mode: %d\n", st.st_mode);
-        printf("st_size: %d\n", st.st_size);
-        //print the name of the file
-        printf("entry->d_name: %s\n", entry->d_name);
-
-        // Populate the metadata structure
-        Metadata metadata;
-        strncpy(metadata.name, entry->d_name, MAX_PATH_LEN);
-        strncpy(metadata.path, entry_path, MAX_PATH_LEN);
-        metadata.type = entry->d_type;
-        metadata.owner = st.st_uid;
-        metadata.group = st.st_gid;
-        metadata.rights = st.st_mode;
-        metadata.content_offset = *data_offset;
-
-        // If the entry is a directory, process it recursively
+    while ((entry = readdir(dir)) != NULL) {
         if (entry->d_type == DT_DIR) {
-            //print that it is a directory
-            printf("DT_DIR\n");
-            store_directory(archive, entry_path, metadata_offset, data_offset);
-        } else if (entry->d_type == DT_REG) {
-            // If the entry is a regular file, store its contents in the archive
-            metadata.size = st.st_size;
-            FILE *input_file = fopen(entry_path, "rb");
-            if (!input_file) {
-                perror("fopen");
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
                 continue;
             }
 
-            // Write the file's content to the archive
-            fseek(archive, * , SEEK_SET);
-            char buffer[1024];
-            size_t bytes_read;
-            while ((bytes_read = fread(buffer, 1, sizeof(buffer), input_file)) > 0) {
-                fwrite(buffer, 1, bytes_read,
-                       archive);
-                *data_offset += bytes_read;
-            }
-            fclose(input_file);
-            //print the file's content
-            printf("file content: %s\n", buffer);
+            char subdir_path[1024];
+            snprintf(subdir_path, sizeof(subdir_path), "%s/%s", dir_path, entry->d_name);
+            traverse_directory(archive_path, subdir_path, base_path, header_offset, metadata_offset, num_entries);
+        } else if (entry->d_type == DT_REG) {
 
-        } else {
-            // If the entry is not a regular file or a directory, skip it
-            continue;
+            char file_path[1024];
+            snprintf(file_path, sizeof(file_path), "%s/%s", dir_path, entry->d_name);
+            const char *relative_path = file_path + strlen(base_path) - 4;
+            write_file_to_archive(archive_path, relative_path, header_offset, metadata_offset, num_entries);
+
+
+
+            printf("file written to archive: %s\n", relative_path);
+            //(*file_count)++;
         }
-
-        // Write the metadata to the archive
-        fseek(archive, *metadata_offset, SEEK_SET);
-        fwrite(&metadata, sizeof(Metadata), 1, archive);
-        *metadata_offset += sizeof(Metadata);
-        //print that it has been written to the archive
-        printf("metadata has been written to the archive\n");
-
     }
 
-// Close the directory
     closedir(dir);
 }
 
+void bongo(FILE *archive_path, const char *dir_path, const char *base_path, uint32_t *header_offset, uint32_t *metadata_offset, int* num_entries){
+    DIR *dir = opendir(dir_path);
+    if (dir == NULL) {
+        perror("Error opening directory");
+        exit(EXIT_FAILURE);
+    }
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_DIR) {
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+                continue;
+            }
+
+            char subdir_path[1024];
+            snprintf(subdir_path, sizeof(subdir_path), "%s/%s", dir_path, entry->d_name);
+            bongo(archive_path, subdir_path, base_path, header_offset, metadata_offset, num_entries);
+        } else if (entry->d_type == DT_REG) {
+
+            char file_path[1024];
+            snprintf(file_path, sizeof(file_path), "%s/%s", dir_path, entry->d_name);
+            const char *relative_path = file_path + strlen(base_path) - 4;
+
+            // Get the file size
+            struct stat st;
+            if (stat(file_path, &st) == -1) {
+                perror("Error getting file size");
+                exit(EXIT_FAILURE);
+            }
+            uint32_t file_size = st.st_size;
+
+            //increment metadata offset by file size
+            *metadata_offset += file_size;
+
+
+            //print metadata offset
+            printf("metadata offset: %d\n", *metadata_offset);
+            //(*file_count)++;
+        }
+    }
+
+    closedir(dir);
+}
+
+
+void create_archive(const char *archive_path, const char *dir_path, int* num_entries) {
+
+    // Create a header
+    // Write the header to the archive
+    // Recursively traverse the input directory
+    // For each file or directory in the input directory:
+    //     Create a metadata entry
+    //     Write the metadata entry to the archive
+    //     If the entry is a regular file:
+    //         Write the file content to the archive
+    //     If the entry is a directory:
+    //         Recursively traverse the directory
+    //         For each file or directory in the directory:
+    //             Create a metadata entry
+    //             Write the metadata entry to the archive
+    //             If the entry is a regular file:
+    //                 Write the file content to the archive
+    // Close the archive
+
+    // Create a header
+    Header header;
+    header.signature = HEADER_SIGNATURE;
+    header.num_entries = 0;
+    header.metadata_offset = sizeof(Header);
+
+    //Write the header to the archive
+    FILE *archive_file = fopen(archive_path, "wb");
+    if (archive_file == NULL) {
+        perror("Error opening archive file");
+        exit(EXIT_FAILURE);
+    }
+
+    //write the header to the archive
+    if (fwrite(&header, sizeof(Header), 1, archive_file) != 1) {
+        perror("Error writing to archive");
+        exit(EXIT_FAILURE);
+    }
+
+    //calculate offset of header
+    uint32_t metadata_offset = sizeof(Header);
+    uint32_t header_offset = sizeof(Header);
+
+    bongo(archive_file, dir_path, dir_path, &header_offset, &metadata_offset, num_entries);
+
+
+
+    //traverse the directory specified by input path, and append all file contents to the archive recursively
+    traverse_directory(archive_file, dir_path, dir_path, &header_offset, &metadata_offset, num_entries);
+
+    // Close the archive
+    if (fclose(archive_file) == EOF) {
+        perror("Error closing archive file");
+        exit(EXIT_FAILURE);
+    }
+
+
+
+
+
+}
 int main(int argc, char *argv[]) {
     if (argc < 4) {
         fprintf(stderr, "Usage: %s {-c|-p} <archive_path> <input_path>\n", argv[0]);
@@ -193,67 +268,65 @@ int main(int argc, char *argv[]) {
     const char *flag = argv[1];
     const char *archive_path = argv[2];
     const char *input_path = argv[3];
+    int num_entries = 0;
 
     if (strcmp(flag, "-c") == 0) {
-        // Create the archive file
-        FILE *archive = fopen(archive_path, "wb");
-        if (!archive) {
-            perror("fopen");
-            return 1;
-        }
 
-        // Write an empty header as a placeholder, will update later
-        Header header = {0};
-        fwrite(&header, sizeof(Header), 1, archive);
 
-        int metadata_offset = sizeof(Header);
-        int data_offset = sizeof(Header);
 
-        // Check if the input is a file or directory
-        struct stat st;
-        if (stat(input_path, &st) == -1) {
-            perror("stat");
-            fclose(archive);
-            return 1;
-        }
+        create_archive(archive_path, input_path, &num_entries);
 
-        if (S_ISDIR(st.st_mode)) {
-            store_directory(archive, input_path, &metadata_offset, &data_offset);
-        } else {
-            // If input is a file, wrap it in a single-entry directory for simplicity
-            char parent_dir[MAX_PATH_LEN];
-            strncpy(parent_dir, input_path, MAX_PATH_LEN);
-            char *last_slash = strrchr(parent_dir, '/');
-            if (last_slash) {
-                *last_slash = '\0';
-            } else {
-                strcpy(parent_dir, ".");
-            }
 
-            store_directory(archive, parent_dir, &metadata_offset, &data_offset);
-        }
 
-        // Update the header with the actual values and write it to the beginning of the file
-        header.signature = HEADER_SIGNATURE;
-        header.num_entries = (metadata_offset - sizeof(Header)) / sizeof(Metadata);
-        header.metadata_offset = metadata_offset;
-        //print the header's metadata
-        printf("header.signature: %d\n", header.signature);
-        printf("header.num_entries: %d\n", header.num_entries);
-        printf("header.metadata_offset: %d\n", header.metadata_offset);
-
-        fseek(archive, 0, SEEK_SET);
-        fwrite(&header, sizeof(Header), 1, archive);
-
-        fclose(archive);
-
+        //return 0;
     } else if (strcmp(flag, "-p") == 0) {
-        display_hierarchy(archive_path);
+        //return 0;
     } else {
         fprintf(stderr, "Unknown flag: %s\n", flag);
         return 1;
+
+
     }
+
+    //read archive and print out the contents
+    FILE *archive_file = fopen(archive_path, "rb");
+    if (archive_file == NULL) {
+        perror("Error opening archive file");
+        exit(EXIT_FAILURE);
+    }
+    //print out the contents
+    Header header;
+    if (fread(&header, sizeof(Header), 1, archive_file) != 1) {
+        perror("Error reading archive");
+        exit(EXIT_FAILURE);
+    }
+
+
+    printf("header signature: %u\n", header.signature);
+    printf("header num_entries: %u\n", header.num_entries);
+    printf("header metadata_offset: %u\n", header.metadata_offset);
+
+    //print the rest of the contents with a buffer
+    char *buffer = malloc(9);
+    if (buffer == NULL) {
+        perror("Error allocating buffer");
+        exit(EXIT_FAILURE);
+    }
+    if (fread(buffer, 9, 1, archive_file) != 1) {
+        perror("Error reading archive");
+        exit(EXIT_FAILURE);
+    }
+    printf("buffer: %s\n", buffer);
+    free(buffer);
+
+
+
 
     return 0;
 
 }
+
+
+
+//traversal is correct.
+//
---
+++ adzip2.c
@@ -0,0 +1,265 @@
+// Include necessary headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+//test
+// Define constants and structures
+#define HEADER_SIGNATURE 0xADADADAD
+#define MAX_PATH_LEN 256
+
+// Header structure for the .ad file
+typedef struct {
+    uint32_t signature;      // A unique signature for .ad file format
+    uint32_t num_entries;    // Number of metadata entries in the archive
+    uint32_t metadata_offset;// Offset to the metadata section in the archive
+} Header;
+
+// Metadata structure for files and directories in the archive
+typedef struct {
+    char name[MAX_PATH_LEN]; // Name of the file or directory
+    uint8_t type;            // Type of the entry (e.g., regular file or directory)
+    char path[MAX_PATH_LEN]; // Relative path of the file or directory
+    uid_t owner;             // Owner user ID
+    gid_t group;             // Group ID
+    mode_t rights;           // Access rights (permissions)
+    uint32_t size;           // Size of the file (0 for directories)
+    uint32_t content_offset; // Offset to the file content in the archive
+} Metadata;
+
+void print_directory(FILE *archive, Metadata *parent_metadata, int depth) {
+    for (int i = 0; i < depth; i++) {
+        printf("  ");
+    }
+    printf("%s\n", parent_metadata->path);
+
+    for (int i = 0; i < parent_metadata->size; i++) {
+        Metadata metadata;
+        fread(&metadata, sizeof(Metadata), 1, archive);
+
+        if (metadata.type == DT_DIR) {
+            print_directory(archive, &metadata, depth + 1);
+        } else {
+            for (int j = 0; j < depth + 1; j++) {
+                printf("  ");
+            }
+            printf("%s\n", metadata.path);
+        }
+    }
+}
+
+void print_hierarchy(FILE *archive) {
+    Header header;
+    fread(&header, sizeof(Header), 1, archive);
+
+    if (header.signature != HEADER_SIGNATURE) {
+        fprintf(stderr, "Invalid archive file.\n");
+        return;
+    }
+    //print the header object
+    printf("header.signature: %d\n", header.signature);
+    printf("header.num_entries: %d\n", header.num_entries);
+    printf("header.metadata_offset: %d\n", header.metadata_offset);
+
+    fseek(archive, header.metadata_offset, SEEK_SET);
+
+    Metadata root_metadata;
+    fread(&root_metadata, sizeof(Metadata), 1, archive);
+
+    if (root_metadata.type == DT_DIR) {
+        print_directory(archive, &root_metadata, 0);
+    } else {
+        printf("%s\n", root_metadata.path);
+    }
+}
+
+void display_hierarchy(const char *archive_path) {
+    FILE *archive = fopen(archive_path, "rb");
+    if (!archive) {
+        perror("fopen");
+        return;
+    }
+
+    printf("Archive: %s\n", archive_path);
+    print_hierarchy(archive);
+
+    fclose(archive);
+}
+
+
+// Function to traverse a directory recursively and store its contents in the archive
+void store_directory(FILE *archive, const char *dir_path, int *metadata_offset, int *data_offset) {
+    // Open the directory
+    DIR *dir = opendir(dir_path);
+    if (!dir) {
+        perror("opendir");
+        return;
+    }
+    printf("dir_path: %s\n", dir_path);
+
+    struct dirent *entry;
+    // Iterate over directory entries
+    while ((entry = readdir(dir))) {
+
+        // Skip "." and ".." entries
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+
+        // Create the full path for the current entry
+        char entry_path[MAX_PATH_LEN];
+        snprintf(entry_path, sizeof(entry_path), "%s/%s", dir_path, entry->d_name);
+        printf("entry_path: %s\n", entry_path);
+
+        // Get the entry's metadata (e.g., file type, owner, group, rights)
+        struct stat st;
+        if (stat(entry_path, &st) == -1) {
+            perror("stat");
+            continue;
+        }
+        //print the entry's metadata
+        printf("st_uid: %d\n", st.st_uid);
+        printf("st_gid: %d\n", st.st_gid);
+        printf("st_mode: %d\n", st.st_mode);
+        printf("st_size: %d\n", st.st_size);
+        //print the name of the file
+        printf("entry->d_name: %s\n", entry->d_name);
+
+        // Populate the metadata structure
+        Metadata metadata;
+        strncpy(metadata.name, entry->d_name, MAX_PATH_LEN);
+        strncpy(metadata.path, entry_path, MAX_PATH_LEN);
+        metadata.type = entry->d_type;
+        metadata.owner = st.st_uid;
+        metadata.group = st.st_gid;
+        metadata.rights = st.st_mode;
+        metadata.content_offset = *data_offset;
+
+        // If the entry is a directory, process it recursively
+        if (entry->d_type == DT_DIR) {
+            //print that it is a directory
+            printf("DT_DIR\n");
+            store_directory(archive, entry_path, metadata_offset, data_offset);
+        } else if (entry->d_type == DT_REG) {
+            // If the entry is a regular file, store its contents in the archive
+            metadata.size = st.st_size;
+            FILE *input_file = fopen(entry_path, "rb");
+            if (!input_file) {
+                perror("fopen");
+                continue;
+            }
+
+            // Write the file's content to the archive
+            fseek(archive, *data_offset , SEEK_SET);
+            char buffer[1024];
+            size_t bytes_read;
+            while ((bytes_read = fread(buffer, 1, sizeof(buffer), input_file)) > 0) {
+                fwrite(buffer, 1, bytes_read,
+                       archive);
+                *data_offset += bytes_read;
+            }
+            fclose(input_file);
+            //print the file's content
+            printf("file content: %s\n", buffer);
+
+        } else {
+            // If the entry is not a regular file or a directory, skip it
+            continue;
+        }
+
+        // Write the metadata to the archive
+        //fseek(archive, *metadata_offset, SEEK_SET);
+        //fwrite(&metadata, sizeof(Metadata), 1, archive);
+        *metadata_offset += sizeof(Metadata);
+        //print that it has been written to the archive
+        printf("metadata has been written to the archive\n");
+
+        //print metadata offset and data offset
+        printf("metadata_offset: %d\n", *metadata_offset);
+        printf("data_offset: %d\n", *data_offset);
+
+
+    }
+
+// Close the directory
+    closedir(dir);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 4) {
+        fprintf(stderr, "Usage: %s {-c|-p} <archive_path> <input_path>\n", argv[0]);
+        return 1;
+    }
+
+    const char *flag = argv[1];
+    const char *archive_path = argv[2];
+    const char *input_path = argv[3];
+
+    if (strcmp(flag, "-c") == 0) {
+        // Create the archive file
+        FILE *archive = fopen(archive_path, "wb");
+        if (!archive) {
+            perror("fopen");
+            return 1;
+        }
+
+        // Write an empty header as a placeholder, will update later
+        Header header = {0};
+        fwrite(&header, sizeof(Header), 1, archive);
+
+        int metadata_offset = sizeof(Header);
+        int data_offset = sizeof(Header);
+
+        // Check if the input is a file or directory
+        struct stat st;
+        if (stat(input_path, &st) == -1) {
+            perror("stat");
+            fclose(archive);
+            return 1;
+        }
+
+        if (S_ISDIR(st.st_mode)) {
+            store_directory(archive, input_path, &metadata_offset, &data_offset);
+        } else {
+            // If input is a file, wrap it in a single-entry directory for simplicity
+            char parent_dir[MAX_PATH_LEN];
+            strncpy(parent_dir, input_path, MAX_PATH_LEN);
+            char *last_slash = strrchr(parent_dir, '/');
+            if (last_slash) {
+                *last_slash = '\0';
+            } else {
+                strcpy(parent_dir, ".");
+            }
+
+            store_directory(archive, parent_dir, &metadata_offset, &data_offset);
+        }
+
+        // Update the header with the actual values and write it to the beginning of the file
+        header.signature = HEADER_SIGNATURE;
+        header.num_entries = (metadata_offset - sizeof(Header)) / sizeof(Metadata);
+        header.metadata_offset = metadata_offset;
+        //print the header's metadata
+        printf("header.signature: %d\n", header.signature);
+        printf("header.num_entries: %d\n", header.num_entries);
+        printf("header.metadata_offset: %d\n", header.metadata_offset);
+
+        fseek(archive, 0, SEEK_SET);
+        fwrite(&header, sizeof(Header), 1, archive);
+
+        fclose(archive);
+
+    } else if (strcmp(flag, "-p") == 0) {
+        display_hierarchy(archive_path);
+    } else {
+        fprintf(stderr, "Unknown flag: %s\n", flag);
+        return 1;
+    }
+    //print_archive_contents(archive_path);
+
+    return 0;
+
+}
